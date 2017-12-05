0b970b96481f60cdedae13f53bf1694dc6e3622b
phurbalama <phurbalama2002@gmail.com>
2017-12-05 18:24:40 -0500

    Add files via upload

---
+++ onlinelibrary/inc/data.php
@@ -0,0 +1,196 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Book of Joy",
+	"img" => "img/media/bookofjoy.jpg",
+    "genre" => "Self-help",
+    "format" => "Paperback",
+    "year" => 2016,
+    "category" => "Books",
+    "authors" => [
+        "Desmond Tutu",
+        "14th Dalai Lama",
+        "Douglas Abrams"
+    ],
+    "publisher" => "Penguin Publishing Group",
+    "isbn" => '9780399185045'
+];
+
+$catalog[102] = [
+	"title" => "Harry Potter and the Deathly Hallows",
+	"img" => "img/media/harrypotter.jpg",
+    "genre" => "Fantasy",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "J. K. Rowling"
+    ],
+    "publisher" => "Bloomsbury",
+    "isbn" => '0-545-01022-5'
+];
+
+$catalog[103] = [
+	"title" => "A Design Patterns: Elements of Reusable Object-Oriented Software",
+	"img" => "img/media/oop.jpg",
+    "genre" => "Tech",
+    "format" => "Paperback",
+    "year" => 1994,
+    "category" => "Books",
+    "authors" => [
+        "Erich Gamma",
+        "Richard Helm",
+        "Ralph Johnson",
+        "John Vlissides"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0201633610'
+];
+
+$catalog[104] = [
+	"title" => "IT",
+	"img" => "img/media/IT.jpg",
+    "genre" => "Horror",
+    "format" => "Paperback",
+    "year" => 1986,
+    "category" => "Books",
+    "authors" => [
+        "Stephen King"
+    ],
+    "publisher" => "Viking",
+    "isbn" => '0-670-81302-8'
+];
+
+
+
+
+
+//Movies
+$catalog[201] = [
+    "title" => "Avatar",
+    "img" => "img/media/avatar.jpg",
+    "genre" => "Action,Adventure,Fantasy",
+    "format" => "DVD",
+    "year" => 2009,
+    "category" => "Movies",
+    "director" => "James Cameron",
+    "writers" => [
+        "James Cameron"
+
+    ],
+    "stars" => [
+        "Sam Worthington",
+        "Zoe Saldana",
+        "Sigourney Weaver"
+    ]
+];
+
+$catalog[202] = [
+    "title" => "Braveheart",
+    "img" => "img/media/braveheart.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 1995,
+    "category" => "Movies",
+    "director" => "Mel Gibson",
+    "writers" => [
+        "Randall Wallace"
+
+    ],
+    "stars" => [
+        "Mel Gibson",
+        "Sophie Marceau",
+        "Patrick McGoohan"
+
+    ]
+];
+
+
+
+$catalog[203] = [
+    "title" => "Everest",
+    "img" => "img/media/everestt.jpg",
+    "genre" => "Action,Adventure,Drama",
+    "format" => "DVD",
+    "year" => 2015,
+    "category" => "Movies",
+    "director" => "Baltasar Kormakur",
+    "writers" => [
+        "William Nicholson",
+        "Simon Beaufoy"
+    ],
+    "stars" => [
+        "Json Clarke",
+        "Ang Phula Sherpa",
+        "Thomas M. Wright"
+    ]
+];
+
+
+
+$catalog[204] = [
+    "title" => "Get Out",
+    "img" => "img/media/getout.jpg",
+    "genre" => "Horror, thriller",
+    "format" => "DVD",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "Jordan Peele",
+    "writers" => [
+        "Jordan Peele"
+    ],
+    "stars" => [
+        "Daniel Kaluuya",
+        "Allison Williams",
+        "Dradley Whitford"
+
+    ]
+];
+
+
+
+
+//Music
+$catalog[301] = [
+    "title" => "Beethoven: Complete Symphonies",
+    "img" => "img/media/beethoven.jpg",
+    "genre" => "Clasical",
+    "format" => "CD",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "Ludwig van Beethoven"
+];
+
+$catalog[302] = [
+    "title" => "The Best- Reminiscent 10th Anniversary",
+    "img" => "img/media/yiruma.jpg",
+    "genre" => "New age",
+    "format" => "CD",
+    "year" => 2011,
+    "category" => "Music",
+    "artist" => "Yiruma"
+];
+
+
+$catalog[303] = [
+    "title" => "Scrublife",
+    "img" => "img/media/waxAlbum.jpg",
+    "genre" => "Hip-hop/rap",
+    "format" => "CD",
+    "year" => 2011,
+    "category" => "Music",
+    "artist" => "Wax"
+];
+
+
+$catalog[304] = [
+    "title" => "Babel",
+    "img" => "img/media/babel.jpg",
+    "genre" => "Fol rock, indie folk",
+    "format" => "CD",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "Mumford & Sons"
+];
+?>
